4b6adbec53b488dbaa41250bca4a26c8f50be350
xpg94 <ejubkadric@gmail.com>
2016-04-12 10:27:20 +0200

    added some template design

--- app/serializers/restaurant.js
+++ app/serializers/restaurant.js
@@ -1,9 +1,22 @@
 import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend({
-    
+/*
+    normalizeFindAllResponse(store, primaryModelClass, payload, id, requestType) {
+
+        payload.restaurants.forEach((restaurant) => {
+                city = restaurant.coordinates.city;
+                address = restaurant.address;
+
+        });
+
+        return this._super(...arguments);
+    },
+*/
+
     normalizeFindAllResponse(store, type, payload){
         return {
+            primaryKey: 'socialSecurityNumber',
             data: {
                 type: type.modelName,
                 id: "restaurantId",
