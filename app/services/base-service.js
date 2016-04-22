e8b19acb6e554bf2b25710e3cfc6d1d943be94dd
xpg94 <ejubkadric@gmail.com>
2016-04-22 16:11:33 +0200

    registration added

--- app/services/base-service.js
+++ app/services/base-service.js
@@ -1,7 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+    authToken: null,
+    currentUser: null,
+
+    setAuthToken: function (token) {
+      this.set('authToken', token);
+    },
+
     ajax: function(params) {
-        return Ember.$.ajax(params);
+      if(this.get('authToken')){
+        Ember.merge(params,{
+          headers: {
+            "USER-ACCESS-TOKEN":this.get("authToken"),
+          }
+        });
+      }
+
+      return Ember.$.ajax(params);
     }
 });
--- app/services/user-service.js
+++ app/services/user-service.js
@@ -2,6 +2,8 @@ import base from './base-service';
 
 
 export default base.extend({
+
+
   loginUser: function (email, password) {
     return this.ajax({
       method: 'POST',
@@ -11,4 +13,29 @@ export default base.extend({
       dataType: 'json',
     });
   },
+
+  getCurrentUser: function () {
+    return this.ajax({
+      method: 'GET',
+      url: '/v1/getCurrentUser',
+      contentType: "application/json",
+
+    });
+  },
+
+  setCurrentUser: function (user) {
+    this.set('currentUser', user);
+  },
+
+  setCurrentUserToken: function (token) {
+    window.localStorage.setItem('USER-ACCESS-TOKEN', token);
+    localStorage.setItem('USER-ACCESS-TOKEN', token);
+    this.set('authToken', token);
+    alert("Inside user-service, token is "+token);
+  },
+
+  removeCurrentUserToken: function () {
+    localStorage.removeItem('USER-ACCESS-TOKEN');
+    this.set('authToken', null);
+  }
 });
